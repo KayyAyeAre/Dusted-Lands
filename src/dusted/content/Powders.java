8f382c3747e7fdf1d979be0a9a7ee4208a9f196f
Chawanya-c <66300552+Chawanya-c@users.noreply.github.com>
2022-04-04 13:19:22 +0700

    aaaaaa why did it use that account

--- src/dusted/content/Powders.java
+++ src/dusted/content/Powders.java
@@ -4,9 +4,10 @@ import arc.graphics.*;
 import dusted.type.*;
 
 public class Powders {
-    public static Powder titaniumPowder;
+    public static Powder titaniumPowder, quartzDust;
 
     public static void load() {
         titaniumPowder = new Powder("titanium-powder", Color.valueOf("8da1e3"));
+        quartzDust = new Powder("quartz-dust");
     }
 }
