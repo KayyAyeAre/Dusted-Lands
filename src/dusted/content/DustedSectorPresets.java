8b4768c346deea5e1cdf1804821f843a8cfb9e1e
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-09-28 20:50:20 +0300

    Update DustedSectorPresets.java

--- src/dusted/content/DustedSectorPresets.java
+++ src/dusted/content/DustedSectorPresets.java
@@ -3,7 +3,7 @@ package dusted.content;
 import mindustry.type.*;
 
 public class DustedSectorPresets {
-    public static SectorPreset outbreak, taintedValley;
+    public static SectorPreset outbreak, taintedValley, corruptedIslands, cariousBulwark;
 
     public static void load() {
         outbreak = new SectorPreset("outbreak", DustedPlanets.krakai, 269) {{
@@ -15,6 +15,15 @@ public class DustedSectorPresets {
         taintedValley = new SectorPreset("tainted-valley", DustedPlanets.krakai, 37) {{
             captureWave = 10;
             difficulty = 2;
+        }};
+
+        corruptedIslands = new SectorPreset("corrupted-islands", DustedPlanets.krakai, 127) {{
+            captureWave = 20;
+            difficulty = 3;
+        }};
+
+        cariousBulwark = new SectorPreset("carious-bulwark", DustedPlanets.krakai, 24) {{
+            difficulty = 4;
         }};
     }
 }
