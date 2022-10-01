3f9ff6ff7996417d37165e6a26510125262a44b1
Chawanya-c <kayayeare16@gmail.com>
2022-10-01 19:44:24 +0700

    fixed #5

--- src/dusted/content/DustedLoadouts.java
+++ src/dusted/content/DustedLoadouts.java
@@ -1,11 +1,19 @@
 package dusted.content;
 
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
 import mindustry.game.*;
+import mindustry.world.blocks.storage.*;
 
 public class DustedLoadouts {
     public static Schematic basicAbate;
 
     public static void load() {
         basicAbate = Schematics.readBase64("bXNjaAF4nBXKQQ6AIAwF0Y81bjyAp+BExkWhXZAgNRTj9dVk8lYDAhHmxqeC7DGsop57uUaxBmCpnLQ6pv0I2OT2oRIrN/GYrWvkxEO/L/x9vDVAE8w=");
+
+        //aaaAAAAAA
+        Reflect.<ObjectMap<CoreBlock, Seq<Schematic>>>get(Vars.schematics, "loadouts").get((CoreBlock) DustedBlocks.coreAbate, Seq::new).add(basicAbate);
+        Reflect.<ObjectMap<CoreBlock, Schematic>>get(Vars.schematics, "defaultLoadouts").put((CoreBlock) DustedBlocks.coreAbate, basicAbate);
     }
 }
